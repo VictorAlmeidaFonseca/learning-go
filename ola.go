845105cf798b9928d4d07096f4386c971d357a9d
Victor Fonseca <victor.fonseca@linkapi.com.br>
2020-09-18 22:59:24 -0300

    add newmatch package

--- ola.go
+++ ola.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/user/newmath"
+)
 
 func main() {
-	fmt.Printf("olá mundo. \n")
+	fmt.Printf("olá mundo. Sqrt(2) = %v\n", newmath.Sqrt(2))
 }
